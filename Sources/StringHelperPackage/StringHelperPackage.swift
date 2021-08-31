8516a8d426b5811074a157b87c3c1ef67e82ea02
Kareem Chanfreau <kareem.chanfreau@gmail.com>
2021-08-31 11:14:33 -0500

    Initial Commit

---
+++ Sources/StringHelperPackage/StringHelperPackage.swift
@@ -0,0 +1,18 @@
+import Foundation
+ 
+public struct StringHelperPackage {
+    
+    func localizedString(_ string: String) -> String {
+        string.localized()
+    }
+    
+    func joinAttributedStrings(_ strings: NSAttributedString...) -> NSAttributedString {
+        return NSAttributedString(string: strings.map { $0.string }.joined())
+    }
+}
+ 
+extension String {
+    public func localized(comment: String = "") -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+}
